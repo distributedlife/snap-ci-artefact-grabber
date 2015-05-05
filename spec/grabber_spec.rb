48b669734a67353b14c51fa08b465679e3bd827d
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-05-05 14:27:04 +1000

    added tests for getting the artefact contents

--- spec/grabber_spec.rb
+++ spec/grabber_spec.rb
@@ -8,7 +8,7 @@ describe 'SnapCIArtefactGrabber' do
   fake_pipeline_response = {
     _embedded: {
       pipelines: [{
-        counter: 1
+        counter: 88
       }
       ]
     }
@@ -32,11 +32,28 @@ describe 'SnapCIArtefactGrabber' do
       stub_env('SNAP_USER', 'distributedlife')
       stub_env('SNAP_APIKEY', 'password')
 
-      FakeWeb.register_uri(:any, "https://distributedlife:password@api.snap-ci.com/project/owner/repo/branch/branch/pipelines", :body => fake_pipeline_response.to_json)
+      FakeWeb.register_uri(:any, 'https://distributedlife:password@api.snap-ci.com/project/owner/repo/branch/branch/pipelines', :body => fake_pipeline_response.to_json)
     end
 
     it 'should get the latest pipeline information' do
       expect{artefact_grabber}.to_not raise_error
+    end
+
+    describe :get_artefact_url_for_stage do
+      before :each do
+        FakeWeb.register_uri(:any, 'https://distributedlife:password@api.snap-ci.com/project/owner/repo/branch/branch/artifacts/pipeline/88/stage/1/filename', :body => "file contents")
+      end
+
+      it 'should construct the url' do
+        expect(artefact_grabber).to receive(:get_from_snap).with('https://api.snap-ci.com/project/owner/repo/branch/branch/artifacts/pipeline/88/stage/1/filename')
+
+        artefact_grabber.get_artefact_url_for_stage "stage", "filename"
+      end
+
+      it 'should return the file content' do
+        response = artefact_grabber.get_artefact_url_for_stage "stage", "filename"
+        expect(response).to eq "file contents"
+      end
     end
   end
 end
